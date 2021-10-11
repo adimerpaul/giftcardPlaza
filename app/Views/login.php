cc9fb7854e80b8dc3263bd33b51a8dfec4825db1
mikky02 <92340605+mikky02@users.noreply.github.com>
2021-10-11 18:38:14 -0400

    modifique la cabezera

--- app/Views/login.php
+++ app/Views/login.php
@@ -18,7 +18,7 @@
                         <div class="row justify-content-center">
                             <div class="col-lg-5">
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
-                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Login Tarjeta</h3></div>
+                                    <div class="card-header"><h3 class="text-center font-weight-light my-4">Ingreso Tarjeta</h3></div>
                                     <div class="card-body">
                                     <?php if(session()->getFlashdata('msg')):?>
                                         <div class="alert alert-warning">
